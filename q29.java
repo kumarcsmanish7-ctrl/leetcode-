33d95d61ab83691811d6d1c14780a07e9c742207
kumarcsmanish7-ctrl <kumarcsmanish7@gmail.com>
2026-05-11 23:13:12 +0530

    update

---
+++ q29.java
@@ -0,0 +1,84 @@
+// 2770. Maximum Number of Jumps to Reach the Last Index
+// You are given a 0-indexed array nums of n integers and an integer target.
+
+// You are initially positioned at index 0. In one step, you can jump from index i to any index j such that:
+
+// 0 <= i < j < n
+// -target <= nums[j] - nums[i] <= target
+// Return the maximum number of jumps you can make to reach index n - 1.
+
+// If there is no way to reach index n - 1, return -1.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,6,4,1,2], target = 2
+// Output: 3
+// Explanation: To go from index 0 to index n - 1 with the maximum number of jumps, you can perform the following jumping sequence:
+// - Jump from index 0 to index 1. 
+// - Jump from index 1 to index 3.
+// - Jump from index 3 to index 5.
+// It can be proven that there is no other jumping sequence that goes from 0 to n - 1 with more than 3 jumps. Hence, the answer is 3. 
+// Example 2:
+
+// Input: nums = [1,3,6,4,1,2], target = 3
+// Output: 5
+// Explanation: To go from index 0 to index n - 1 with the maximum number of jumps, you can perform the following jumping sequence:
+// - Jump from index 0 to index 1.
+// - Jump from index 1 to index 2.
+// - Jump from index 2 to index 3.
+// - Jump from index 3 to index 4.
+// - Jump from index 4 to index 5.
+// It can be proven that there is no other jumping sequence that goes from 0 to n - 1 with more than 5 jumps. Hence, the answer is 5. 
+// Example 3:
+
+// Input: nums = [1,3,6,4,1,2], target = 0
+// Output: -1
+// Explanation: It can be proven that there is no jumping sequence that goes from 0 to n - 1. Hence, the answer is -1. 
+
+class Solution {
+    Integer dp[];
+    public int maximumJumps(int[] nums, int target) {
+        dp = new Integer[nums.length];
+        return helper(0,nums,target);
+    }
+    int helper(int ind , int[] nums, int target){
+        if(ind==nums.length-1){
+            return 0;
+        }
+        if(dp[ind]!= null){
+            return dp[ind];
+
+        }
+        int jumps =-1 ;
+        for(int i =ind+1; i<nums.length;i++ ){
+            if(Math.abs(nums[i]-nums[ind])<=target){
+                int next = helper(i, nums, target);
+                if(next!= -1){
+                jumps  =Math.max(jumps,next+1);
+            }
+            }
+            
+        }return dp[ind]= jumps;
+    }
+}
+
+
+    //iterative approach , bottom up approach
+    public int maximumJumps(int[] nums, int target) {
+        int n =nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        dp[n-1]=0;
+        for(int ind= n-1;ind>=0;ind-- ){
+            for(int i = ind+1;i<n;i++){
+                if(Math.abs(nums[i]-nums[ind])<=target){
+                    if(dp[i]!=-1){
+                        dp[ind]=Math.max(dp[ind],1+dp[i]);
+                    }
+                }
+            }
+        }
+        return dp[0];
+    }
